b635c00df56a9ea3e7fd9f1f7b32d53e6545b262
Ja1rman <49319500+Ja1rman@users.noreply.github.com>
2020-06-01 21:28:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # -*- coding: utf8 -*-
 import telebot
 from telebot import types
+from datetime import datetime
+import pymysql
 
 bot = telebot.TeleBot('1178505929:AAEwS2trEjBsMuUyT_7BG0dxuz2MjcZ309o', threaded=False)
 
@@ -8,9 +10,56 @@ bot = telebot.TeleBot('1178505929:AAEwS2trEjBsMuUyT_7BG0dxuz2MjcZ309o', threaded
 def welcome(message):
     bot.send_message(message.chat.id, "Просто отправьте сообщение и оно появится на канале!")
 
+@bot.message_handler(commands=['donate'])
+def money(message):
+    bot.send_message(message.chat.id, "За платной подпиской обращайтесь к Odmen_228!")
+
 @bot.message_handler(content_types=['text', 'photo'])
 def answer(message):
-    bot.forward_message('@buyaccountgame', message.chat.id, message.message_id)
+    chat_id = message.chat.id
+    now = datetime.today()
+    
+    con = pymysql.connect('eu-cdbr-west-03.cleardb.net', 'ba181f0e44fd71', '322a1c67', 'heroku_babf1d4d975c9ec')
+    with con:
+        cur = con.cursor()
+
+        cur.execute("SELECT sub FROM members WHERE chat_id = %s", (chat_id,))
+        row = cur.fetchall()
+        cur.execute("SELECT date FROM members WHERE chat_id = %s", (chat_id,))
+        now2 = cur.fetchall()
+        if len(row) == 0:
+            cur.execute("INSERT INTO members (chat_id, date, sub, sub_date) VALUES (%s, %s, %s, %s)", (chat_id, now, 0, now))
+            bot.forward_message('@buyaccountgame', chat_id, message.message_id)
+        else:
+            delta = now - now2
+            seconds = delta.seconds
+            minutes = seconds // 60
+
+            if int(row) == 1:
+                cur.execute("SELECT sub_date FROM members WHERE chat_id = %s", (chat_id,))
+                now_don = cur.fetchall()
+                delta_don = now - now_don
+                seconds_don = delta_don.seconds
+                days = seconds_don // 86400
+                if days < 30:
+                    if minutes >= 10:
+                        bot.forward_message('@buyaccountgame', chat_id, message.message_id)
+                        cur.execute("UPDATE members SET date = ? WHERE chat_id = ?", (now, chat_id))
+                    else:
+                        bot.send_message(chat_id, 'Для отправки сообщения подождите ещё ' + str(10 - minutes) + 'минут')
+                else:
+                    bot.send_message(chat_id, 'Ваша подписка закончилась((( Приобретите её снова командой /donate')
+                    cur.execute("UPDATE members SET sub = ? WHERE chat_id = ?", (0, chat_id))
+            else:
+                hours = seconds // 3600
+                if hours >= 1:
+                    bot.forward_message('@buyaccountgame', chat_id, message.message_id)
+                    cur.execute("UPDATE members SET date = ? WHERE chat_id = ?", (now, chat_id))
+                else:
+                    bot.send_message(chat_id, 'Для отправки сообщения подождите ещё ' + str(60 - minutes) + 'минут')
+                    bot.send_message(chat_id, 'Для снятия ограничений на отправку приобретите премиум версию командой /donate')
+
+        cur.close()
 
 while True:
     try:
